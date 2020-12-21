53f89219f190f54ab630aa9b5d9a7d5c0b564a53
Stickolas <nick95th@gmail.com>
2020-12-20 23:09:10 -0600

    gushers

---
+++ Source/KittyForce/KittyForceGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "KittyForceGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AKittyForceGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AKittyForceGameMode();
+};
+
+
+
